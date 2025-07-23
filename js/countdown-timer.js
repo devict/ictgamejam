e2efcbe58670083da98849c4a1e7c233514440c7
Jack Hale <misterjackhale@gmail.com>
2025-07-23 00:11:45 -0500

    update date, location, countdowntimer, and splash; add photos of winners

--- js/countdown-timer.js
+++ js/countdown-timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("July 18, 2025 17:30:00").getTime();
+var countDownDate = new Date("January 30, 2026 19:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
