18a462f997c2ccc6a8f5e7d749b05ff4287bbf1c
Jack Hale <misterjackhale@gmail.com>
2026-01-31 16:54:50 -0600

    revert erroneous countdown change

--- js/countdown-timer.js
+++ js/countdown-timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("January 30, 2026 12:00:00").getTime();
+var countDownDate = new Date("January 30, 2026 11:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
